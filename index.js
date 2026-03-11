1536920392871d173db0a9a79639d269f453f18d
SomeOne <NEUTRONWAR34@email.com>
2026-03-11 14:00:02 -0500

    npm run dev

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-'use strict';
-const http = require('http');
-const server = http.createServer(function(req, res)
-{
-res.writeHead(200, {'content-type': 'text/plain'});
-res.end('hola mundo');
-});
-server.listen(5000);
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/', (req, res) => {
+    res.send('Hola')
+
+})
+
+
+app.listen(port, () => {
+    console.log('La aplicacion se esta ejecutando por el puerto')
+
+})
